c6a5b4b2b4d5fb7c84c19e6090793236de68df2e
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-06-27 14:41:04 +0530

    function of the === in comparison

--- JS/app.js
+++ JS/app.js
@@ -10,15 +10,12 @@
 // let n1 = 20;
 // let n2 = 20;
 
-
-
 // console.log(n1 + n2);
 
 // alert("click me ")
 
 // let a = "BMW"
 // console.log(typeof a);
-
 
 // let b = true
 // console.log(typeof b);
@@ -30,20 +27,22 @@
 // let e = 'c'
 // console.log(typeof e);
 
-let name = "saman";
+// let name = "saman";
 
-let age = 18;
+// let age = 18;
 
-let isAfterAl = false;
+// let isAfterAl = false;
 
-let  customers = [];
+// let  customers = [];
 
-let customers = {};
-let salary = 7500.0;
+// let customers = {};
+// let salary = 7500.0;
 
-console.log(typeof name);
+// console.log(typeof name);
 
+////////////////////////////////////////////////////////////////////////
 
+let x = 10;
+let y = "10";
 
-
-
+console.log(x === y); // tripe '=' cheks the comparison and also the variabel type.
