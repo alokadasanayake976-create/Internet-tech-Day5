2df1ad0a08ddcd9efb4d3802336a08a8f2968403
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-06-27 13:01:03 +0530

    console log of the js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,17 @@
-console.log("A-RON")
+// console.log("A-RON")
+let num1 = "Aloka";
+
+console.log(num1);
+
+let num2 = 12;
+
+console.log(num2);
+
+let n1 = 20;
+let n2 = 20;
+
+
+
+console.log(n1 + n2);
+
+alert("click me ")
