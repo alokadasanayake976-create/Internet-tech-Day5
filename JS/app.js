dd07cf39af4ba7b885df668634605c790013f414
alokadasanayake976-create <alokadasanayake976@gmail.com>
2026-06-27 14:28:20 +0530

    typeof keyward

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,49 @@
 // console.log("A-RON")
-let num1 = "Aloka";
+// let num1 = "Aloka";
 
-console.log(num1);
+// console.log(num1);
 
-let num2 = 12;
+// let num2 = 12;
 
-console.log(num2);
+// console.log(num2);
 
-let n1 = 20;
-let n2 = 20;
+// let n1 = 20;
+// let n2 = 20;
 
 
 
-console.log(n1 + n2);
+// console.log(n1 + n2);
 
-alert("click me ")
+// alert("click me ")
+
+// let a = "BMW"
+// console.log(typeof a);
+
+
+// let b = true
+// console.log(typeof b);
+
+// let c = 10
+// console.log(typeof c);
+// let d = 1.2
+// console.log(typeof d);
+// let e = 'c'
+// console.log(typeof e);
+
+let name = "saman";
+
+let age = 18;
+
+let isAfterAl = false;
+
+let  customers = [];
+
+let customers = {};
+let salary = 7500.0;
+
+console.log(typeof name);
+
+
+
+
+
